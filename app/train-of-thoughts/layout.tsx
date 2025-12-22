2af37304b69533fda7a42344aa41bf3bd3b6d2dc
Harshat Dhanayat <dhanayat.harshat@gmail.com>
2025-12-22 22:26:24 +0530

    [Modifications] Changed cursor to none and added cursor animations for Train of Thoughts pages

--- app/train-of-thoughts/layout.tsx
+++ app/train-of-thoughts/layout.tsx
@@ -1,10 +1,13 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+// import { Inter } from "next/font/google"
+import { Roboto_Mono } from "next/font/google"
 import "../globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import CustomCursor from "@/components/custom-cursor"
 
-const inter = Inter({ subsets: ["latin"] })
+// const inter = Inter({ subsets: ["latin"] })
+const robotoMono = Roboto_Mono({ subsets: ["latin"], variable: "--font-roboto-mono" })
 
 export const metadata: Metadata = {
   title: "Harshat Dy | Train of Thoughts",
@@ -18,8 +21,9 @@ export default function TrainOfThoughtsLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={robotoMono.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <CustomCursor />
           {children}
         </ThemeProvider>
       </body>
